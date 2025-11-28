d271b1c86b08d4a2090796e6f0239faaa82945c3
Ozgur <ozgurbayram@protonmail.com>
2025-11-29 02:02:18 +0300

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Analytics } from "@vercel/analytics/next";
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import ChargePointConnection from './pages/ChargePointConnection';
 import Dashboard from './pages/Dashboard';
-
 const App = () => {
   return (
     <HashRouter>
+      <Analytics />
       <Routes>
         <Route path='/' element={<Dashboard />} />
         <Route path='/cp/:id' element={<ChargePointConnection />} />
